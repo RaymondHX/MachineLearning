1f13c87b0f87b4036d76607def85a326f8ab7b3d
RaymondHX <33855254+RaymondHX@users.noreply.github.com>
2019-10-20 15:13:20 +0800

    完善逻辑回归

--- LogisticsRegression.py
+++ LogisticsRegression.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import math as math
 import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn.datasets.samples_generator import make_classification
 
 #建立逻辑回归的数据集（自己手动生成的数据集）
 def create_data1():
@@ -12,13 +13,36 @@ def create_data1():
     y = [[1], [1], [1], [1], [1], [1], [1], [0], [0], [0], [0], [0]]
     dataset = np.hstack((x, y))
     return dataset
+#从sklearn库里面的make_classfication
+def create_data2():
+    X, labels = make_classification(n_samples=200, n_features=2, n_redundant=0, n_informative=2,
+                                    random_state=1, n_clusters_per_class=1)
+    dataset = np.hstack((X, np.mat(labels).T))
+    return  dataset
+
+#高斯分布的数据（利用协方差矩阵判断是否满足朴素贝叶斯）
+def create_data3():
+    mean1 = [0, 0]
+    cov1 = [[1, 0], [0, 1]]
+    data1 = np.random.multivariate_normal(mean1, cov1, 100)
+    mean2 = [3, 3]
+    cov2 = [[1, 0], [0, 1]]
+    data2 = np.random.multivariate_normal(mean2, cov2, 100)
+    zero = np.mat(np.zeros(100)).T
+    data1 = np.hstack((data1, zero))
+    one = np.mat(np.ones(100)).T
+    data2 = np.hstack((data2, one))
+    data =np.vstack((data1, data2))
+    np.random.shuffle(data)
+    return data
+
 
 #从ucl数据集中得到数据
 def get_ucldata():
     data =pd.read_csv("data_banknote_authentication.csv")
     data = data.values
     np.random.shuffle(data)
-    print(data)
+    #print(data)
     return data
 
 def sigmoid(x):
@@ -30,8 +54,9 @@ def hloss(x, y, W):
     loss = -np.dot(y.T, np.log(sigmoid(np.dot(x, W.T)))+1e-5) + np.dot((1 - y).T, np.log(1 - sigmoid(np.dot(x, W.T))+1e-5))
     return  loss/m
 
-#利用梯度下降法求参数
-def gradien_desent(x,y,learning_rate):
+
+#利用梯度下降法求参数（regu参数true/false确定是否加正则项）
+def gradien_desent(x,y,learning_rate,regu = False):
     x = np.column_stack((np.ones(np.size(x, 0)).T, x))
     n = np.size(x, 1)
     m = np.size(x, 0)
@@ -39,9 +64,12 @@ def gradien_desent(x,y,learning_rate):
     old_loss = 0
     loss = hloss(x, y, W.T)
     loss_diff = loss-old_loss
-    while(math.fabs(loss_diff)>2e-8):
+    while(math.fabs(loss_diff)>1e-8):
         error = sigmoid(np.dot(x, W)) - y
-        W = W - learning_rate/m*np.dot(x.T, error)
+        if(regu):
+            W = W - learning_rate / m * np.dot(x.T, error)-learning_rate*W*0.8
+        else:
+            W = W - learning_rate/m*np.dot(x.T, error)
         old_loss = loss
         loss = hloss(x, y, W.T)
         loss_diff = loss-old_loss
@@ -59,17 +87,22 @@ def judge(x,y,W):
             if (y[i]==0):
                 result = result + 1
     return result/m
-#画图
+#画图(二维)
 def drawDomension2(W, data, string):
-    x1 = np.linspace(0, 15, 10000)
+    x1 = np.linspace(-5, 5, 10000)
     x2 = (W[0, 0]+W[1, 0]*x1)/(-W[2, 0])
-    plt.plot(data[0:7, 0].T, data[0:7, 1].T, "ro")
-    plt.plot(data[7:12, 0].T, data[7:12, 1].T, "go")
+    for i in range(np.size(data,0)):
+        if(data[i, 2]==0):
+             plt.plot(data[i, 0], data[i, 1], "ro")
+        else:
+            plt.plot(data[i, 0].T, data[i, 1], "go")
     plt.plot(x1, x2)
+    plt.title(string)
     plt.show()
 
+#画图（三维）
 def drawDimension3(W, data):
-    print(np.shape(data))
+  #  print(np.shape(data))
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     x1 = []
@@ -94,13 +127,29 @@ def drawDimension3(W, data):
 
 if __name__ == '__main__':
      data = create_data1()
-     W = gradien_desent(data.T[np.arange(0, 2)].T, data.T[np.arange(2, 3)].T, 0.001)
-     drawDomension2(W, data, "hhh")
+     W = gradien_desent(data.T[np.arange(0, 2)].T, data.T[np.arange(2, 3)].T, 0.001, False)
+     drawDomension2(W, data, "自己手动生成数据")
+     print("自己手动生成数据的正确率(无正则化)")
+     print(judge(np.column_stack((np.ones(np.size(data.T[np.arange(0, 2)].T, 0)).T,
+                                  data.T[np.arange(0, 2)].T)), data.T[np.arange(2, 3)].T, W))
+     W = gradien_desent(data.T[np.arange(0, 2)].T, data.T[np.arange(2, 3)].T, 0.001, True)
+     drawDomension2(W, data, "自己手动生成数据")
+     print("自己手动生成数据的正确率(正则化)")
      print(judge(np.column_stack((np.ones(np.size(data.T[np.arange(0, 2)].T, 0)).T,
                                   data.T[np.arange(0, 2)].T)), data.T[np.arange(2, 3)].T, W))
      data = get_ucldata()
      W = gradien_desent(data[0:1000, 0:4], data[0:1000, 4:5], 0.001)
+     print("UCI上数据集的正确率")
      print(judge(np.column_stack((np.ones(np.size(data[1000:1300, 0:4], 0)).T, data[1000:1300, 0:4])), data[1000:1300, 4:5], W))
      drawDimension3(W, data)
-
-
+     data = create_data2()
+     W = gradien_desent(data.T[np.arange(0, 2)].T, data.T[np.arange(2, 3)].T, 0.001)
+     drawDomension2(W, data, "利用sklearn库生成的数据")
+     print("利用sklearn库生成的数据的正确率")
+     print(judge(np.column_stack((np.ones(np.size(data.T[np.arange(0, 2)].T, 0)).T,
+                             data.T[np.arange(0, 2)].T)), data.T[np.arange(2, 3)].T, W))
+     data = create_data3()
+     W = gradien_desent(data[0:80, 0:2], data[0:80, 2:3], 0.001, False)
+     print("两个独立的高斯分布生成的数据的正确率")
+     print(judge(np.column_stack((np.ones(np.size(data[80:100, 0:2], 0)).T, data[80:100, 0:2])), data[80:100, 2:3], W))
+     drawDomension2(W, data, "两个独立的高斯分布")
